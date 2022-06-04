4810f1abd6076b3cc32a472533883551404a0a73
csw <csw@elonsoft.co.kr>
2022-06-04 20:55:23 +0900

    후위연산 변환 클래스 수정

--- src/calc/FormulaChange.java
+++ src/calc/FormulaChange.java
@@ -1,56 +1,67 @@
 package calc;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class FormulaChange {
     // 중위연산 -> 후위연산
     // 에러처리 필수
-    Queue<Double> numbers = new LinkedList<>();
-    Queue<String> operators = new LinkedList<>();
-    List<String> postfix = new ArrayList<>();
+    Stack<String> output = new Stack<>();
+    Stack<String> operators = new Stack<>();
+    String postfix = "";
 
-    public List<String> toPostfix(String formula) {
+    public String toPostfix(String formula) {
 
         int i = 0;
 
-        // 연산자랑 한자리 숫자는 괜찮은데 숫자가 2자리 이상일 때 처리로직 필요함
+        // 연산자랑 한자리 숫자는 괜찮은데 숫자가 2자리 이상일 때 처리로직 필요함 (흠 ..)
         while(i < formula.length()) {
             String letter = formula.substring(i, i+1);
+
+            // 돌아다니면서 순서대로 곱셈 나눗셈 먼저 빼온다
+            // 다시 돌아다니면서 순서대로 덧셈 뺄셈 빼온다
+            // 처음에만 2개 숫자 빼고, 나머지 경우는 1개만 빼면 됨
+
             if (CalcUtil.isNumber(letter)) {
-                numbers.add(Double.parseDouble(letter));
+                output.push(letter);
             }
 
             if (CalcUtil.isOperator(letter)) {
-                operators.add(letter);
+                checkOperator(letter);
             }
 
             i++;
         }
 
-        return joinToPostfix();
-    }
-
-    // 후위연산식으로 결합
-    private List<String> joinToPostfix() {
-        while(true) {
-            // 말도 안 됨 ..
-            Double num1 = numbers.remove();
-            Double num2 = numbers.remove();
-            String op = operators.remove();
-
-            postfix.add(num1.toString());
-            postfix.add(num2.toString());
-            postfix.add(op);
-
-            if (numbers.isEmpty() && operators.isEmpty()) {
-                break;
+        // 연산자 스택에 남아있는 연산자가 있을 시
+        if (operators.size() > 0) {
+            for (int j=0; j<operators.size(); j++) {
+                output.push(operators.pop());
             }
         }
-        return postfix;
+
+        return "";
     }
+
+    // 연산자 체크 로직
+    public void checkOperator(String op) {
+        if (operators.isEmpty()) {
+            operators.push(op);
+            return;
+        }
+
+        if (operators.peek().equals("*") && op.equals("+")) {
+            for (int i=0; i<operators.size(); i++) {
+                output.push(operators.pop());
+            }
+        }
+
+        if (operators.peek().equals(op)) {
+            output.push(operators.pop());
+        }
+
+    }
+
+
 
 
 }
